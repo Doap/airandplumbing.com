bc3295d045863214e61051db9ee86786b5695cdf
David Menache <dave@doap.com>
2015-03-04 22:40:06 +0000

    minor changes to airandplumbing.com:

--- scripts/make_softlinks.sh
+++ scripts/make_softlinks.sh
@@ -6,4 +6,5 @@ ln -s /srv/www/uploads/sinkjuice/airandplumbing.com/uploads/ /var/www/html/airan
 ln -s /srv/www/uploads/sinkjuice/airandplumbing.com/themes/ /var/www/html/airandplumbing.com/wp-content/themes
 ln -s /srv/www/uploads/sinkjuice/airandplumbing.com/plugins/ /var/www/html/airandplumbing.com/wp-content/plugins
 cp -f /startup/footer.php.airandplumbing.com /var/www/html/airandplumbing.com/wp-content/themes/AandP-Child/footer.php 
+/startup/genvhost.sh airandplumbing.com
 echo "/startup/RunResourceTest-airandplumbing.com.sh just ran on `curl -s http://169.254.169.254/latest/meta-data/public-ipv4`." | tee /tmp/startup.log
