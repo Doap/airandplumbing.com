3555160ff5dbebc6a7927bd6584b7d66c56ef5d6
David Menache <dave@doap.com>
2015-03-04 22:49:26 +0000

    minor changes to airandplumbing.com: added-pre-remove-wp-config.sh

--- scripts/make_softlinks.sh
+++ scripts/make_softlinks.sh
@@ -7,4 +7,5 @@ ln -s /srv/www/uploads/sinkjuice/airandplumbing.com/themes/ /var/www/html/airand
 ln -s /srv/www/uploads/sinkjuice/airandplumbing.com/plugins/ /var/www/html/airandplumbing.com/wp-content/plugins
 cp -f /startup/footer.php.airandplumbing.com /var/www/html/airandplumbing.com/wp-content/themes/AandP-Child/footer.php 
 /startup/genvhost.sh airandplumbing.com
+/startup/make-wp-configs airandplumbing.com
 echo "/startup/RunResourceTest-airandplumbing.com.sh just ran on `curl -s http://169.254.169.254/latest/meta-data/public-ipv4`." | tee /tmp/startup.log
