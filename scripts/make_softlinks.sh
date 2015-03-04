15122a79d3133581a8cf8693702974f23980d830
David Menache <dave@doap.com>
2015-03-04 22:11:03 +0000

    minor changes to airandplumbing.com: softlinks-gen-in-yml

--- scripts/make_softlinks.sh
+++ scripts/make_softlinks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-mv /var/www/html/airandplumbing.com/wp-content/uploads /var/www/html/airandplumbing.com/wp-content/uploads.bak
-mv /var/www/html/airandplumbing.com/wp-content/plugins /var/www/html/airandplumbing.com/wp-content/plugins.bak
-mv /var/www/html/airandplumbing.com/wp-content/themes /var/www/html/airandplumbing.com/wp-content/themes.bak
+#mv /var/www/html/airandplumbing.com/wp-content/uploads /var/www/html/airandplumbing.com/wp-content/uploads.bak
+#mv /var/www/html/airandplumbing.com/wp-content/plugins /var/www/html/airandplumbing.com/wp-content/plugins.bak
+#mv /var/www/html/airandplumbing.com/wp-content/themes /var/www/html/airandplumbing.com/wp-content/themes.bak
 ln -s /srv/www/uploads/sinkjuice/airandplumbing.com/uploads/ /var/www/html/airandplumbing.com/wp-content/uploads
 ln -s /srv/www/uploads/sinkjuice/airandplumbing.com/themes/ /var/www/html/airandplumbing.com/wp-content/themes
 ln -s /srv/www/uploads/sinkjuice/airandplumbing.com/plugins/ /var/www/html/airandplumbing.com/wp-content/plugins
