dc56f1edccc37ab21f40332db88ab58f36b495ef
David Menache <dave@doap.com>
2015-03-05 00:08:56 +0000

    minor changes to airandplumbing.com: softlinks-gen-in-yml

--- scripts/make_softlinks.sh
+++ scripts/make_softlinks.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+if [ ! -d /var/www/html/airandplumbing.com/wp-content/themes ] 
+then
+    mkdir -p /var/www/html/airandplumbing.com/wp-content/themes
+fi
+
+if [ ! -d /var/www/html/airandplumbing.com/wp-content/plugins ] 
+then
+    mkdir -p /var/www/html/airandplumbing.com/wp-content/plugins
+fi
+
 ln -s /srv/www/uploads/sinkjuice/airandplumbing.com/uploads/ /var/www/html/airandplumbing.com/wp-content/uploads
 ln -s /srv/www/uploads/sinkjuice/airandplumbing.com/themes/ /var/www/html/airandplumbing.com/wp-content/themes
 ln -s /srv/www/uploads/sinkjuice/airandplumbing.com/plugins/ /var/www/html/airandplumbing.com/wp-content/plugins
